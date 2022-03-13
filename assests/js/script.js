5e7f8ad9a71e0d08f0a66d4e649960e11d9f6995
Amanda Dell <amhernandez1163@gmail.com>
2022-03-13 13:04:28 -0500

    attempting to save tasks text

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,24 +1,24 @@
-var tasks = {};
+var taskInput = $("#tasks").text.val();
+var saveButton = $("#saveBtn");
 
 var addDate = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").text(addDate);
 
 
-var saveBtn = function () {
-    $(".saveBtn").on("click", function() {
-        console.log("this click was saved.")
-    }); 
-};
+$(function () {
+    alert(taskInput);
+});
 
-    
+saveButton.click(function() {
+    console.log("text was saved");
+});
 
-var displayTasks = function () {
+var getTasks = function () {
     tasks = JSON.parse(localStorage.getItem("tasks"))
 }
 
 var saveTasks = function() {
-    localStorage.setItem("tasks", );
-    console.log("this does something");
-  };
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
 
 
