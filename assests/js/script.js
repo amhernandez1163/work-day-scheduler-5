5bbd3e9f3658f11c4735635500bcf904f0234740
Amanda Dell <amhernandez1163@gmail.com>
2022-03-13 16:17:47 -0500

    added event listener to each button for each text area - beginning to work on local storage

--- assests/js/script.js
+++ assests/js/script.js
@@ -3,27 +3,106 @@ var tasks = $("#tasksInput").val();
 var addDate = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").text(addDate);
 
+$(document).ready(function() {
+    $("#one").click(function() {
+   
+        var getTaskInput = $.trim($("#tasksInput1").val());
+        if(localStorage.getItem("text") === null){
+            localStorage.setItem("text", JSON.stringify(getTaskInput));
+        }
+        
+        //var taskText = JSON.parse(localStorage.getItem("text"));
+       //taskText.push(tasks);
+        
+        //localStorage.setItem("text", JSON.stringify(getTaskInput));
+    });
+});
 
-var saveTasks = function () {
+$(document).ready(function() {
+    $("#two").click(function() {
+   
+        var getTaskInput = $.trim($("#tasksInput2").val());
+        if(localStorage.getItem("text") === null){
+            localStorage.setItem("text", JSON.stringify(getTaskInput));
+        }
+    });
+});
 
-    if(localStorage.getItem("text") === null){
-        localStorage.setItem("text", "tasks");
-    }
+$(document).ready(function() {
+    $("#three").click(function() {
+   
+        var getTaskInput = $.trim($("#tasksInput3").val());
+        console.log(getTaskInput);
+    });
+});
 
-    var taskText = JSON.parse(localStorage.getItem("text"));
-    taskText.push(tasks);
+$(document).ready(function() {
+    $("#four").click(function() {
+   
+        var getTaskInput = $.trim($("#tasksInput4").val());
+        console.log(getTaskInput);
+    });
+});
 
-    localStorage.setItem("text", JSON.stringify(tasks));
+$(document).ready(function() {
+    $("#five").click(function() {
+   
+        var getTaskInput = $.trim($("#tasksInput5").val());
+        console.log(getTaskInput);
+    });
+});
 
- saveTasks();
-};
+$(document).ready(function() {
+    $("#six").click(function() {
+   
+        var getTaskInput = $.trim($("#tasksInput6").val());
+        console.log(getTaskInput);
+    });
+});
 
+$(document).ready(function() {
+    $("#seven").click(function() {
+   
+        var getTaskInput = $.trim($("#tasksInput7").val());
+        console.log(getTaskInput);
+    });
+});
 
+$(document).ready(function() {
+    $("#eight").click(function() {
+   
+        var getTaskInput = $.trim($("#tasksInput8").val());
+        console.log(getTaskInput);
+    });
+});
 
-$(".saveBtn").click(function() {
-    console.log("click registered");
+$(document).ready(function() {
+    $("#nine").click(function() {
+   
+        var getTaskInput = $.trim($("#tasksInput9").val());
+        console.log(getTaskInput);
+    });
+});
+
+$(document).ready(function() {
+    $("#ten").click(function() {
+   
+        var getTaskInput = $.trim($("#tasksInput10").val());
+        console.log(getTaskInput);
+    });
 });
 
 
+//$(".saveBtn").click(function() {
+  ////  getTaskInput();
+//});
 
+
+
+
+//var getTaskInput = function () {
+    
+  //  var taskInput = document.getElementById("tasksInput").value;
+
+//};
 
