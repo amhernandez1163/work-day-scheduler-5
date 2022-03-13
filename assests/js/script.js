74e0411cc659bc1149014412fc27b999cb4abb7b
Amanda Dell <amhernandez1163@gmail.com>
2022-03-13 01:07:08 -0600

    got date to work. stopping point for night - starting on save button

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,6 +1,24 @@
 var tasks = {};
-var displayTime = $("#currentDay");
 
 var addDate = moment().format('MMMM Do YYYY, h:mm:ss a');
-$("#currentDay").html(addDate);
+$("#currentDay").text(addDate);
+
+
+var saveBtn = function () {
+    $(".saveBtn").on("click", function() {
+        console.log("this click was saved.")
+    }); 
+};
+
     
+
+var displayTasks = function () {
+    tasks = JSON.parse(localStorage.getItem("tasks"))
+}
+
+var saveTasks = function() {
+    localStorage.setItem("tasks", );
+    console.log("this does something");
+  };
+
+
